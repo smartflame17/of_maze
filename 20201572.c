6dfbe174ee9e0b50d61afe2e41a26fe1c672e0a8
smartflame <jasonjiseop.kim@gmail.com>
2022-05-30 17:07:10 +0900

    fixed loop appearance

--- 20201572.c
+++ 20201572.c
@@ -17,182 +17,205 @@ void EndMaze(int row);                                      //Deals with last ro
 int RandBool();
 int main()
 {
-    FILE* fp1;
-    FILE* fp2;
-    int row, col, i;
+	FILE* fp;
+	int row, col, i;
 
-    fp1 = fopen("input.txt", "r");
-    fp2 = fopen("maze.txt", "w");
-    fscanf(fp1, "%d", &row);
-    fscanf(fp1, "%d", &col);
+	fp = fopen("maze.maz", "w");
+	printf("Input row size of maze : ");
+	scanf("%d", &row);
+	printf("Input column size of maze : ");
+	scanf("%d", &col);
 
-    MazeRow = (int*)malloc(sizeof(int)*row);           
-    RowConnect = (int*)malloc(sizeof(int)*(row-1));
-    ColConnect = (int*)malloc(sizeof(int)*row);
+	MazeRow = (int*)malloc(sizeof(int)*row);
+	RowConnect = (int*)malloc(sizeof(int)*(row - 1));
+	ColConnect = (int*)malloc(sizeof(int)*row);
 
-    srand(time(NULL));
-    ////////////////////////////////////////////////////////// Actual creation & drawing of maze part
-    DrawWall(row, &fp2);
-    InitMaze(row);
-    DrawRow(row, &fp2);
-    DrawBottom(row, &fp2);
-    for (i = 0; i < col-2; i++){
-        CreateLine(row);
-        DrawRow(row, &fp2);
-        DrawBottom(row, &fp2);
-    }
-    EndMaze(row);
-    DrawRow(row, &fp2);
-    DrawWall(row, &fp2);
-    //////////////////////////////////////////////////////////
-    fclose(fp1);
-    fclose(fp2);
-    free(MazeRow);
-    free(RowConnect);
-    free(ColConnect);
+	srand(time(NULL));
+	////////////////////////////////////////////////////////// Actual creation & drawing of maze part
+	DrawWall(row, &fp);
+	InitMaze(row);
+	DrawRow(row, &fp);
+	DrawBottom(row, &fp);
+	for (i = 0; i < col - 2; i++) {
+		CreateLine(row);
+		DrawRow(row, &fp);
+		DrawBottom(row, &fp);
+	}
+	EndMaze(row);
+	DrawRow(row, &fp);
+	DrawWall(row, &fp);
+	//////////////////////////////////////////////////////////
+	fclose(fp);
+	free(MazeRow);
+	free(RowConnect);
+	free(ColConnect);
 }
-void DrawWall(int row, FILE** fp){
-    for (int i = 0; i < row; i++)
-        fprintf(*fp, "+-");
-    fprintf(*fp, "+\n");
-}
-
-void DrawRow(int row, FILE** fp){
-    fprintf(*fp, "|");
-    for (int i = 0; i < row-1; i++){
-        //fprintf(*fp, "%d", MazeRow[i]);               debugging
-        fprintf(*fp, " ");                              //Print one cell of maze
-        if (RowConnect[i])
-            fprintf(*fp, " ");                          //If cell is connected
-        else fprintf(*fp, "|");
-    }
-    //fprintf(*fp, "%d|\n", MazeRow[row-1]);            debugging
-    fprintf(*fp, " |\n");
+void DrawWall(int row, FILE** fp) {
+	for (int i = 0; i < row; i++)
+		fprintf(*fp, "+-");
+	fprintf(*fp, "+\n");
 }
 
-void DrawBottom(int row, FILE** fp){
-    for (int i = 0; i < row; i++){
-        fprintf(*fp, "+");
-        if (ColConnect[i])
-            fprintf(*fp, " ");              //If col is connected, bottom cell has same index
-        else {
-            fprintf(*fp, "-");
-            MazeRow[i] = -1;                //Otherwise initialized to -1 (addressed in CreateLine)
-        }
-    }
-    fprintf(*fp, "+\n");
+void DrawRow(int row, FILE** fp) {
+	fprintf(*fp, "|");
+	for (int i = 0; i < row - 1; i++) {
+		//fprintf(*fp, "%d", MazeRow[i]);               //debugging
+		fprintf(*fp, " ");                              //Print one cell of maze
+		if (RowConnect[i])
+			fprintf(*fp, " ");                          //If cell is connected
+		else fprintf(*fp, "|");
+	}
+	//fprintf(*fp, "%d|\n", MazeRow[row - 1]);            //debugging
+	fprintf(*fp, " |\n");
 }
 
-void InitMaze(int row){
-    int i, bottom_check_flag = 0;                   //bottom_check_flag makes sure at least 1 bottom hole exists for each index
-    int currentIndex;                               //tracks current index of cell for bottom hole check
-
-    CellIndex = 0;
-    MazeRow[0] = CellIndex;
-    CellIndex++;
-    for(i = 1; i < row; i++){
-        if (RandBool()){                            //Decide to merge 2 cells
-            RowConnect[i-1] = TRUE;                 //TRUE means no walls between cells
-            MazeRow[i] = MazeRow[i-1];
-        }
-        else{
-            RowConnect[i-1] = FALSE;                //FALSE means wall between cells
-            MazeRow[i] = CellIndex;                 //Newly indexed block
-            CellIndex++;
-        }
-    }
-
-    currentIndex = MazeRow[0];
-    for (i = 0; i < row; i++){
-        ColConnect[i] = FALSE;                          //Default is FALSE
-        if (MazeRow[i] != currentIndex){                //If new indexed cell comes, reset flag and currentIndex
-            currentIndex = MazeRow[i];
-            bottom_check_flag = 0;
-        }
-        if (!bottom_check_flag){                        //no holes yet
-            if (i == row - 1)                           //if last cell still has no hole, make one
-                ColConnect[i] = TRUE;               
-            else if (MazeRow[i+1] != currentIndex){     //look ahead into next cell, if last cell with index
-                ColConnect[i] = TRUE;
-            }
-            else if (RandBool()){
-                ColConnect[i] = TRUE;
-                bottom_check_flag = 1;
-            }
-        }
-        else if (RandBool()){                           //already has hole, then randomly decide
-                ColConnect[i] = TRUE;
-                bottom_check_flag = 1;
-        }
-    }
+void DrawBottom(int row, FILE** fp) {
+	for (int i = 0; i < row; i++) {
+		fprintf(*fp, "+");
+		if (ColConnect[i])
+			fprintf(*fp, " ");              //If col is connected, bottom cell has same index
+		else {
+			fprintf(*fp, "-");
+			MazeRow[i] = -1;                //Otherwise initialized to -1 (addressed in CreateLine)
+		}
+	}
+	fprintf(*fp, "+\n");
 }
 
-void CreateLine(int row){
-    int i, bottom_check_flag = 0;
-    int currentIndex;
+void InitMaze(int row) {
+	int i, bottom_check_flag = 0;                   //bottom_check_flag makes sure at least 1 bottom hole exists for each index
+	int currentIndex;                               //tracks current index of cell for bottom hole check
 
-    for (i = 0; i < row; i++){                  //For unconnected new cells, assign new index
-        if (MazeRow[i] == -1){
-            MazeRow[i] = CellIndex;
-            CellIndex++;
-        }
-    }
-    for (i = 0; i < row-1; i++){                //Deals with RowConnect
-        if (MazeRow[i] != MazeRow[i+1]){        //if two cells have different index
-            RowConnect[i] = FALSE;
-            if (RandBool()){
-                MazeRow[i+1] = MazeRow[i];      //Randomly merge two cells
-                RowConnect[i] = TRUE;
-            }
-        }
-        else{
-            RowConnect[i] = FALSE;              //if same index, do not connect
-        }
-    }
-    /////
-    currentIndex = MazeRow[0];
-    for (i = 0; i < row; i++){
-        ColConnect[i] = FALSE;                          //Default is FALSE
-        if (MazeRow[i] != currentIndex){                //If new indexed cell comes, reset flag and currentIndex
-            currentIndex = MazeRow[i];
-            bottom_check_flag = 0;
-        }
-        if (!bottom_check_flag){                        //no holes yet
-            if (i == row - 1)                           //if last cell still has no hole, make one
-                ColConnect[i] = TRUE;               
-            else if (MazeRow[i+1] != currentIndex){     //look ahead into next cell, if last cell with index
-                ColConnect[i] = TRUE;
-            }
-            else if (RandBool()){
-                ColConnect[i] = TRUE;
-                bottom_check_flag = 1;
-            }
-        }
-        else if (RandBool()){                           //already has hole, then randomly decide
-                ColConnect[i] = TRUE;
-                bottom_check_flag = 1;
-        }
-    }
+	CellIndex = 0;
+	for (i = 0; i < row; i++) {
+		MazeRow[i] = CellIndex;
+		CellIndex++;
+	}
+	for (i = 1; i < row; i++) {
+		if (RandBool()) {                            //Decide to merge 2 cells
+			RowConnect[i - 1] = TRUE;                 //TRUE means no walls between cells
+			MazeRow[i] = MazeRow[i - 1];
+		}
+		else RowConnect[i - 1] = FALSE;
+	}
+
+	currentIndex = MazeRow[0];
+	for (i = 0; i < row; i++) {
+		ColConnect[i] = FALSE;                          //Default is FALSE
+		if (MazeRow[i] != currentIndex) {                //If new indexed cell comes, reset flag and currentIndex
+			currentIndex = MazeRow[i];
+			bottom_check_flag = 0;
+		}
+		if (!bottom_check_flag) {                        //no holes yet
+			if (i == row - 1)                           //if last cell still has no hole, make one
+				ColConnect[i] = TRUE;
+			else if (MazeRow[i + 1] != currentIndex) {     //look ahead into next cell, if last cell with index
+				ColConnect[i] = TRUE;
+			}
+			else if (RandBool()) {
+				ColConnect[i] = TRUE;
+				bottom_check_flag = 1;
+			}
+		}
+		else if (RandBool()) {                           //already has hole, then randomly decide
+			ColConnect[i] = TRUE;
+			bottom_check_flag = 1;
+		}
+	}
 }
 
-void EndMaze(int row){
-    int i;
+void CreateLine(int row) {
+	int i, j, bottom_check_flag = 0;
+	int currentIndex;
 
-    for (i = 0; i < row; i++){                  //For unconnected new cells, assign new index
-        if (MazeRow[i] == -1){
-            MazeRow[i] = CellIndex;
-            CellIndex++;
-        }
-    }
-    for (i = 0; i < row-1; i++){
-        if (MazeRow[i] != MazeRow[i+1]){        //If different index, merge
-            MazeRow[i+1] = MazeRow[i];
-            RowConnect[i] = TRUE;
-        }
-        else RowConnect[i] = FALSE;
-    }
+	for (i = 0; i < row; i++) {                  //For unconnected new cells, assign new index
+		if (MazeRow[i] == -1) {
+			MazeRow[i] = CellIndex;
+			CellIndex++;
+		}
+	}
+	for (i = 0; i < row - 1; i++) {                //Deals with RowConnect
+		if (MazeRow[i] == MazeRow[i + 1]) {        //if two cells have same index
+			RowConnect[i] = FALSE;
+		}
+	}
+	for (i = 0; i < row - 1; i++) {
+		RowConnect[i] = FALSE;
+		if (MazeRow[i] != MazeRow[i + 1]) {
+			RowConnect[i] = FALSE;              //if different index, randomly connect
+			if (RandBool()) {
+				int tmp = MazeRow[i + 1];
+				MazeRow[i + 1] = MazeRow[i];      //Randomly merge two cells
+				for (j = 0; j < row; j++) {
+					if (MazeRow[j] == tmp)
+						MazeRow[j] = MazeRow[i];
+				}
+				RowConnect[i] = TRUE;
+			}
+		}
+	}
+	for (i = 0; i < row - 1; i++) {
+		if (RowConnect[i]) {
+			MazeRow[i + 1] = MazeRow[i];
+		}
+	}
+	/////
+	currentIndex = MazeRow[0];
+	for (i = 0; i < row; i++) {
+		ColConnect[i] = FALSE;                          //Default is FALSE
+		if (MazeRow[i] != currentIndex) {                //If new indexed cell comes, reset flag and currentIndex
+			currentIndex = MazeRow[i];
+			bottom_check_flag = 0;
+		}
+		if (!bottom_check_flag) {                        //no holes yet
+			if (i == row - 1)                           //if last cell still has no hole, make one
+				ColConnect[i] = TRUE;
+			else if (MazeRow[i + 1] != currentIndex) {     //look ahead into next cell, if last cell with index
+				ColConnect[i] = TRUE;
+			}
+			else if (RandBool()) {
+				ColConnect[i] = TRUE;
+				bottom_check_flag = 1;
+			}
+		}
+		else if (RandBool()) {                           //already has hole, then randomly decide
+			ColConnect[i] = TRUE;
+			bottom_check_flag = 1;
+		}
+	}
 }
 
-int RandBool(){
-    return rand()%2;
+void EndMaze(int row) {
+	int i, j;
+
+	for (i = 0; i < row; i++) {                  //For unconnected new cells, assign new index
+		if (MazeRow[i] == -1) {
+			MazeRow[i] = CellIndex;
+			CellIndex++;
+		}
+	}
+	for (i = 0; i < row - 1; i++) {
+		if (MazeRow[i] == MazeRow[i + 1]) {        //if two cells have same index
+			RowConnect[i] = FALSE;
+		}
+		else {
+			RowConnect[i] = TRUE;
+			int tmp = MazeRow[i + 1];
+			MazeRow[i + 1] = MazeRow[i];      
+			for (j = 0; j < row; j++) {
+				if (MazeRow[j] == tmp)
+					MazeRow[j] = MazeRow[i];
+			}
+		}
+	}
+	/*for (i = 0; i < row - 1; i++) {
+		if (MazeRow[i] != MazeRow[i + 1]) {        //If different index, merge
+			MazeRow[i + 1] = MazeRow[i];
+			RowConnect[i] = TRUE;
+		}
+	}*/
+}
+
+int RandBool() {
+	return rand() % 2;
 }
